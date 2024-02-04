e47f5478e1aa1801521d6042bc772232cca5adcd
Saffana Khan <149876002+saffanakhan20@users.noreply.github.com>
2024-02-04 18:37:54 +0530

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,1 +1,20 @@
+# Task 1
+# Rule Based Chatbot
+# Chatbot rules
+rules = {
+    "What is your name?": "My name is Chatbot.",
+    "How are you?": "I'm doing well, thank you!",
+    "What is capital of India?": "Capital of India is New Delhi.",
+}
 
+# Logic
+def chatbot_response(user_input):
+    for pattern, response in rules.items():
+        if pattern in user_input:
+            return response
+    return "I'm sorry, I don't understand."
+
+# Testing chatbot
+user_input = input("User: ")
+chatbot_output = chatbot_response(user_input)
+print("Chatbot:", chatbot_output)
